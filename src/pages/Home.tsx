58691487dfab8ef4a70c08a5cd478f980e440775
sciforearth <sciforearth@gmail.com>
2024-12-13 22:03:37 +0530

    main page update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 import { ArrowDown, Code2, Sparkles } from 'lucide-react';
 import ValuesSection from '../components/home/ValuesSection';
 import ImpactSection from '../components/home/ImpactSection';
+import Products from './Products';
 
 export default function Home() {
   return (
@@ -44,21 +45,7 @@ export default function Home() {
       <ValuesSection />
       <ImpactSection />
 
-      <section id="products" className="py-20 bg-white">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="text-center mb-16">
-            <Sparkles className="h-12 w-12 text-blue-600 mx-auto mb-4" />
-            <h2 className="text-4xl font-bold text-gray-900 mb-4">Our Products</h2>
-            <p className="text-xl text-gray-600">
-              Discover our suite of innovative software solutions
-            </p>
-          </div>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {/* Product cards will be rendered here */}
-          </div>
-        </div>
-      </section>
+      <Products/>
     </div>
   );
 }
